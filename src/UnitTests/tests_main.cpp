f78db21daf64291634fec3eb385a98a383475131
Souvik Roy <da.comp.guy@gmail.com>
2010-07-05 20:28:20 +0530

    First commit for Unit Tests for Scribe

---
+++ src/UnitTests/tests_main.cpp
@@ -0,0 +1,47 @@
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <fstream>
+#include "servertests.h"
+
+void startServer(void){
+
+	unsigned long int port=1463;
+	std::string config_file="/usr/local/scribe.conf";
+    	g_Handler = boost::shared_ptr<scribeHandler>(new scribeHandler	(port, config_file));
+    	g_Handler->initialize();
+}
+
+int main (int argc, char* argv[])
+{
+	startServer();
+
+    // informs test-listener about testresults
+    CPPUNIT_NS :: TestResult testresult;
+
+    // register listener for collecting the test-results
+    CPPUNIT_NS :: TestResultCollector collectedresults;
+    testresult.addListener (&collectedresults);
+
+    // register listener for per-test progress output
+    CPPUNIT_NS :: BriefTestProgressListener progress;
+    testresult.addListener (&progress);
+
+    // insert test-suite at test-runner by registry
+    CPPUNIT_NS :: TestRunner testrunner;
+    testrunner.addTest (CPPUNIT_NS :: TestFactoryRegistry :: getRegistry ().makeTest ());
+    testrunner.run (testresult);
+
+    // Log Test Results in a log file
+    std::fstream outFile;
+    outFile.open("/tmp/results.txt",std::ios::out);
+    CPPUNIT_NS :: CompilerOutputter compileroutputter (&collectedresults, outFile);
+    compileroutputter.printFailureReport();
+    compileroutputter.write ();
+
+    // return 0 if tests were successful
+    return collectedresults.wasSuccessful () ? 0 : 1;
+}
